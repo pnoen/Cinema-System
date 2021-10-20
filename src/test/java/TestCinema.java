edef257c9497f300dcb5742e1fda7c5e1e4225f7
Will <wqiu8445@uni.sydney.edu.au>
2021-10-20 19:42:27 +1100

    account and login logic tests

---
+++ src/test/java/TestCinema.java
@@ -0,0 +1,109 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.text.ParseException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class TestCinema {
+
+    @Test
+    void testCustomerLoginLogic() {
+        Cinema cinema = new Cinema();
+        assertEquals(false, cinema.getLogged());
+        cinema.setLogged(true);
+
+        String userInput = "2 5";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(inputStream);
+
+        //catching output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        cinema.customerLoginLogic();
+
+        String expected = "You have logged out";
+        String[] output = outputStream.toString().split("\n");
+        String actual = output[output.length-1];
+        assertEquals(expected, actual.trim());
+
+
+    }
+
+    @Test
+    void badLogicInput(){
+        Cinema cinema = new Cinema();
+        cinema.setLogged(true);
+
+        String userInput = "1236 5";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(inputStream);
+
+        //catching output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        cinema.customerLoginLogic();
+
+        String expected = "You have logged out";
+        String[] output = outputStream.toString().split("\n");
+        String actual = output[output.length-1];
+        assertEquals(expected, actual.trim());
+    }
+
+    @Test
+    void testStaffLoginLogic(){
+        Cinema cinema = new Cinema();
+        cinema.setLogged(true);
+
+        String userInput = "6";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(inputStream);
+
+        //catching output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        cinema.staffLoginLogic();
+
+        String expected = "You have logged out";
+        String[] output = outputStream.toString().split("\n");
+        String actual = output[output.length-1];
+        assertEquals(expected, actual.trim());
+    }
+
+    @Test
+    void testManagerLoginLogic(){
+        Cinema cinema = new Cinema();
+        cinema.setLogged(true);
+
+        String userInput = "8";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(inputStream);
+
+        //catching output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        cinema.managerLoginLogic();
+
+        String expected = "You have logged out";
+        String[] output = outputStream.toString().split("\n");
+        String actual = output[output.length-1];
+        assertEquals(expected, actual.trim());
+    }
+
+
+
+
+
+
+
+}
